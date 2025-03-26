0359d2bc82bcdc6b25b3f7af163e304a5f945b00
Julia Yuan, UofC <jyuan@ucalgary.ca>
2025-03-26 09:35:01 -0600

    added coverage and multisimulation options

--- LDAR_Sim/DataProcessing/LDAR_Sim_result_processing.py
+++ LDAR_Sim/DataProcessing/LDAR_Sim_result_processing.py
@@ -12,9 +12,10 @@ def regional_LDARSim_resultprocessing():
     ogi_persite_high_df = pd.DataFrame()
     ogi_sensor_df = pd.DataFrame()
     johnson_df = pd.DataFrame()
+    coverage_df = pd.DataFrame()
     all_tech_df = pd.DataFrame()
 
-    simulation_versions = ['basecase', 'crew_count', 'mdl', 'site_num', 'site_num_180', 'ogi_persite_high', 'ogi_sensor', 'triannual', 'johnson']
+    simulation_versions = ['basecase', 'coverage','crew_count', 'mdl', 'site_num', 'site_num_180', 'ogi_persite_high', 'ogi_sensor', 'triannual', 'johnson']
     regionslist = ['BV', 'DV', 'EDM','GP','MDP', 'MH', 'RD','SL','WW']
     for simulation in simulation_versions:
         for region in regionslist:
@@ -109,6 +110,18 @@ def regional_LDARSim_resultprocessing():
                 ogi_sensor_df = pd.concat([ogi_sensor_df, annual_regional_combined_data.head(1)])
                 all_tech_df = pd.concat([all_tech_df, annual_regional_combined_data])
 
+            elif simulation == 'coverage':
+                datapath_path = f'C:/Users/jyuan/OneDrive/Documents/GitHub/LDAR_Sim/LDAR_Sim/outputs/sensitivity/{simulation}/annual_LDARSim4_{region}/Cost Summary.csv'
+                annual_regional_raw_data = pd.read_csv(datapath_path)
+                annual_regional_combined_data = annual_regional_raw_data.groupby('Program Name').mean()
+                annual_regional_combined_data = annual_regional_combined_data.sort_values(
+                    by=['Mitigation Ratio ($/tonne CO2e)'])
+                annual_regional_combined_data = annual_regional_combined_data.reset_index(names=['Program Name'])
+                annual_regional_combined_data['Region'] = region
+                annual_regional_combined_data['Simulation'] = simulation
+                coverage_df = pd.concat([coverage_df, annual_regional_combined_data.head(1)])
+                all_tech_df = pd.concat([all_tech_df, annual_regional_combined_data])
+
 
             elif simulation == 'johnson':
                 datapath_path = f'C:/Users/jyuan/OneDrive/Documents/GitHub/LDAR_Sim/LDAR_Sim/outputs/{simulation}/annual_LDARSim4_{region}/Cost Summary.csv'
@@ -122,53 +135,52 @@ def regional_LDARSim_resultprocessing():
                 johnson_df = pd.concat([johnson_df, annual_regional_combined_data.head(1)])
                 all_tech_df = pd.concat([all_tech_df, annual_regional_combined_data])
 
-    ldarsim_simulation_all = pd.concat([basecase_df, triannual_df, crew_count_df, mdl_df, site_num_df, site_num_180_df, ogi_persite_high_df, ogi_sensor_df, johnson_df])
+    ldarsim_simulation_all = pd.concat([basecase_df, triannual_df, crew_count_df, mdl_df, site_num_df, site_num_180_df, ogi_persite_high_df, ogi_sensor_df, johnson_df, coverage_df])
 
     figure1_difference_regionvscases = ldarsim_simulation_all.pivot(index='Region', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
     simulations_nobasecase = ['crew_count', 'mdl', 'site_num', 'site_num_180', 'ogi_persite_high',
-                           'ogi_sensor', 'triannual', 'johnson']
+                           'ogi_sensor', 'triannual', 'johnson', 'coverage']
     for simulation in simulations_nobasecase:
         figure1_difference_regionvscases[simulation] = figure1_difference_regionvscases[simulation] - figure1_difference_regionvscases['basecase']
 
-    figure1_difference_regionvscases = ldarsim_simulation_all.pivot(index='Region', columns='Simulation', values='Program Name')
     figure1_label_regionvscases = ldarsim_simulation_all.pivot(index='Region', columns='Simulation', values='Program Name')
 
     bv_samplecase = all_tech_df[all_tech_df['Region'] == 'BV']
     bv_samplecase = bv_samplecase.pivot(index='Program Name', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
-    for simulation in simulations_nobasecase:
-        bv_samplecase[simulation] = bv_samplecase[simulation] - bv_samplecase['basecase']
+    # for simulation in simulations_nobasecase:
+    #     bv_samplecase[simulation] = bv_samplecase[simulation] - bv_samplecase['basecase']
     dv_samplecase = all_tech_df[all_tech_df['Region'] == 'DV']
     dv_samplecase = dv_samplecase.pivot(index='Program Name', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
-    for simulation in simulations_nobasecase:
-        dv_samplecase[simulation] = dv_samplecase[simulation] - dv_samplecase['basecase']
+    # for simulation in simulations_nobasecase:
+    #     dv_samplecase[simulation] = dv_samplecase[simulation] - dv_samplecase['basecase']
     edm_samplecase = all_tech_df[all_tech_df['Region'] == 'EDM']
     edm_samplecase = edm_samplecase.pivot(index='Program Name', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
-    for simulation in simulations_nobasecase:
-        edm_samplecase[simulation] = edm_samplecase[simulation] - edm_samplecase['basecase']
+    # for simulation in simulations_nobasecase:
+    #     edm_samplecase[simulation] = edm_samplecase[simulation] - edm_samplecase['basecase']
     gp_samplecase = all_tech_df[all_tech_df['Region'] == 'GP']
     gp_samplecase = gp_samplecase.pivot(index='Program Name', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
-    for simulation in simulations_nobasecase:
-        gp_samplecase[simulation] = gp_samplecase[simulation] - gp_samplecase['basecase']
+    # for simulation in simulations_nobasecase:
+    #     gp_samplecase[simulation] = gp_samplecase[simulation] - gp_samplecase['basecase']
     mdp_samplecase = all_tech_df[all_tech_df['Region'] == 'MDP']
     mdp_samplecase = mdp_samplecase.pivot(index='Program Name', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
-    for simulation in simulations_nobasecase:
-        mdp_samplecase[simulation] = mdp_samplecase[simulation] - mdp_samplecase['basecase']
+    # for simulation in simulations_nobasecase:
+    #     mdp_samplecase[simulation] = mdp_samplecase[simulation] - mdp_samplecase['basecase']
     mh_samplecase = all_tech_df[all_tech_df['Region'] == 'MH']
     mh_samplecase = mh_samplecase.pivot(index='Program Name', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
-    for simulation in simulations_nobasecase:
-        mh_samplecase[simulation] = mh_samplecase[simulation] - mh_samplecase['basecase']
+    # for simulation in simulations_nobasecase:
+    #     mh_samplecase[simulation] = mh_samplecase[simulation] - mh_samplecase['basecase']
     rd_samplecase = all_tech_df[all_tech_df['Region'] == 'RD']
     rd_samplecase = rd_samplecase.pivot(index='Program Name', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
-    for simulation in simulations_nobasecase:
-        rd_samplecase[simulation] = rd_samplecase[simulation] - rd_samplecase['basecase']
+    # for simulation in simulations_nobasecase:
+    #     rd_samplecase[simulation] = rd_samplecase[simulation] - rd_samplecase['basecase']
     sl_samplecase = all_tech_df[all_tech_df['Region'] == 'SL']
     sl_samplecase = sl_samplecase.pivot(index='Program Name', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
-    for simulation in simulations_nobasecase:
-        sl_samplecase[simulation] = sl_samplecase[simulation] - sl_samplecase['basecase']
+    # for simulation in simulations_nobasecase:
+    #     sl_samplecase[simulation] = sl_samplecase[simulation] - sl_samplecase['basecase']
     ww_samplecase = all_tech_df[all_tech_df['Region'] == 'WW']
     ww_samplecase = ww_samplecase.pivot(index='Program Name', columns='Simulation', values='Mitigation Ratio ($/tonne CO2e)')
-    for simulation in simulations_nobasecase:
-        ww_samplecase[simulation] = ww_samplecase[simulation] - ww_samplecase['basecase']
+    # for simulation in simulations_nobasecase:
+    #     ww_samplecase[simulation] = ww_samplecase[simulation] - ww_samplecase['basecase']
 
 
     basecase_df.to_csv('basecase1.csv')
@@ -179,6 +191,7 @@ def regional_LDARSim_resultprocessing():
     site_num_180_df.to_csv('sensitivity_site_num_1801.csv')
     ogi_persite_high_df.to_csv('sensitivity_ogi_persite_high1.csv')
     ogi_sensor_df.to_csv('sensitivity_ogi_sensor1.csv')
+    coverage_df.to_csv('coverage1.csv')
     johnson_df.to_csv('johnson1.csv')
     ldarsim_simulation_all.to_csv('ldarsim_simulation_all.csv')
     figure1_difference_regionvscases.to_csv('heatmap_difference_regionvscases.csv')
@@ -197,8 +210,8 @@ def regional_LDARSim_resultprocessing():
     return
 
 def run_LDAR_sim():
-    simulation_versions = ['site_number' , 'site_number_180'] #'basecase','basecase_emissions_johnson_test','cost_sensitivity','crew_sensitivity','mdl_sensitivity','OGI_sensor', 'triannual_survey',
-    regionslist = [ 'MH', 'RD','SL','WW']#'BV','DV', 'EDM','GP','MDP',
+    simulation_versions = ['coverage'] #'triannual_survey','basecase','site_number','site_number_180' ,'OGI_sensor' 'basecase_emissions_johnson_test','cost_sensitivity','crew_sensitivity','mdl_sensitivity',,,
+    regionslist = [ 'BV','DV', 'EDM','GP','MDP','MH', 'RD','SL','WW']
     for simulation in simulation_versions:
         for region in regionslist:
             if simulation == 'triannual_survey':
